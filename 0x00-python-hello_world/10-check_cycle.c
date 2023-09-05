9d5e7c1aa87012110bc06a2facc42930e07524bb
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-05 05:06:14 +0300

    Hello, Python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - ....
+ *
+ * @head: the list
+ *
+ * Return:0 If there is no cycle.
+ *        1 If there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp1, *temp2;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	temp1 = list;
+	temp2 = list->next;
+
+	while (temp1)
+	{
+		while (temp2)
+		{
+			if (temp1 == temp2)
+				return (1);
+		}
+	}
+
+	return (0);
+}
