8085c7d52ec4235072f3275f07aa367e4c3551a6
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-07 16:02:05 +0300

    Python Import Modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+import sys
+
+args = sys.argv
+
+len = len(args)
+
+if len == 1:
+    print("0 Arguments.")
+elif len == 2:
+    print("1 Argument.")
+    print("1: {}".format[args[1]])
+else
+    print("{} Arguments.".format(len - 1))
+    for i in range(1, len - 1):
+        print("{}: {}".forma(i, args[i]))
