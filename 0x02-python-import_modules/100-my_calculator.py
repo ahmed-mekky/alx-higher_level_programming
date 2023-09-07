88452ea01d287ff737d6bad11603cc183bf36770
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-07 22:49:09 +0300

    Python Import Modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == '__main__':
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    if argv[2] == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    if argv[2] == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    if argv[2] == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
