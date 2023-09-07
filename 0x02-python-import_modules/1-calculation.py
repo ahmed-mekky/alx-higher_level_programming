82f9a2a99f13e15292e18e4f89cd0dd689eba3ef
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-07 15:53:31 +0300

    Python Import Modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as cc
+from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, cc.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, cc.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, cc.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, cc.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
