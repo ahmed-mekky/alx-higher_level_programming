b0705b630391446117a503587967e4e0dc1f32a1
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-07 16:04:21 +0300

    Python Import Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if args_len == 1:
 elif args_len == 2:
     print("1 Argument.")
     print("1: {}".format[args[1]])
-else
+else:
     print("{} Arguments.".format(args_len - 1))
     for i in range(1, args_len):
         print("{}: {}".format(i, args[i]))
