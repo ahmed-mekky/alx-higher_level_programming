f67612e861649c4f539a0158cbee690a50101840
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-07 03:08:56 +0300

    Hello, Python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,13 +14,16 @@ int check_cycle(listint_t *list)
 	listint_t *temp1, *temp2;
 
 	temp1 = list;
-	temp2 = list->next;
 
-	while (temp2)
-	{
-		if (temp1 == temp2)
-			return (1);
-		temp2 = temp2->next;
+	while (temp1) {
+		temp2 = list;
+		while (temp2 != temp1) {
+			if (temp1->next == temp2) {
+				return (1);
+			}
+			temp2 = temp2->next;
+		}
+		temp1 = temp1->next;
 	}
 	return (0);
 }
