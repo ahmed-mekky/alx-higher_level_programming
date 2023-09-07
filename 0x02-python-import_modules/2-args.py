5d9111bbdeff2e3256e04ce593a05592b40f8cfb
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-07 16:03:57 +0300

    Python Import Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@ import sys
 
 args = sys.argv
 
-len = len(args)
+args_len = len(args)
 
-if len == 1:
+if args_len == 1:
     print("0 Arguments.")
-elif len == 2:
+elif args_len == 2:
     print("1 Argument.")
     print("1: {}".format[args[1]])
 else
-    print("{} Arguments.".format(len - 1))
-    for i in range(1, len):
+    print("{} Arguments.".format(args_len - 1))
+    for i in range(1, args_len):
         print("{}: {}".format(i, args[i]))
