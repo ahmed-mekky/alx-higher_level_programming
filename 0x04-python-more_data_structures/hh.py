e222149b73cdf772f2c03db3731107e6545b4190
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 01:41:09 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/hh.py
+++ 0x04-python-more_data_structures/hh.py
@@ -12,7 +12,7 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 roman_number = "IX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "LXXXVII"
+roman_number = None
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
