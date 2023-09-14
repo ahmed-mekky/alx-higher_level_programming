4f7c042dd4162b5a21af0e457a3766bcb5fdc19b
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 06:22:16 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    new_list = sorted(a_dictionary.keys())
-    return new_list[-1]
+    new_list = list(sorted(a_dictionary.values()))
+    for key, value in a_dictionary.items():
+        if (value == new_list[-1]):
+            elkey = key
+    return elkey
