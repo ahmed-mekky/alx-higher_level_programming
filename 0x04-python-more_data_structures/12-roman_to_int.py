c09da68ae5e3ea96705ebb8cff6d372b7070d186
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 01:31:32 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-import roman
 def roman_to_int(roman_string):
-    return roman.fromRoman(roman_string)
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    last_value = 0
+    for num in reversed(roman_string):
+        value = roman_dict[num]
+        if value > last_value:
+            result += value - last_value * 2
+        else:
+            result += value
+        last_value = value
+    return result
