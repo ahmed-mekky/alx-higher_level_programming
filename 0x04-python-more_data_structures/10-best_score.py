96c098326c8fea0923a6cfdc3646393eb99d2559
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 06:07:05 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    new_list = list(sorted(a_dictionary.keys()))
+    new_list = sorted(a_dictionary.keys())
     return new_list[0]
