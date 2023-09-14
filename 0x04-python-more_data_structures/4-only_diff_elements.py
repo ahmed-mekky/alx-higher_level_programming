378213e0948bc2508e4114b2620c2d103c690d7b
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 10:27:30 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_list = list(set_1.difference(set_2))
-    return new_list
+    old_set = set_1.difference(set_2)
+    new_set = set_2.difference(set_1)
+    return old_set.union(new_set)
