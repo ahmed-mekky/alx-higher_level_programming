a0ce7e1d36ea6bc012781431d79be77de28e8d77
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-15 02:23:45 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return None
-    keys = []
-    for key, values in a_dictionary.items():
-        if (values == value):
-            keys.append(key)
-    for key in keys:
-        del a_dictionary[key]
+    if a_dictionary:
+        keys = []
+        for key, values in a_dictionary.items():
+            if values == value:
+                keys.append(key)
+        for key in keys:
+            del a_dictionary[key]
     return a_dictionary
--- 0x04-python-more_data_structures/hh.py
+++ 0x04-python-more_data_structures/hh.py
@@ -11,7 +11,7 @@ print_sorted_dictionary(new_dict)
 
 print("--")
 print("--")
-new_dict = complex_delete(a_dictionary, 'c_is_fun')
+new_dict = complex_delete({}, 'a')
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
