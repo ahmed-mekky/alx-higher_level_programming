98dbe9f320c8df2dbc976e489d738a6c352c5c1a
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 05:58:31 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
+def best_score(a_dictionary):
     new_dict = a_dictionary
-    for item, value in a_dictionary.items():
+    for item, value in a_dictionary:
         item = value **2
     return new_dict
