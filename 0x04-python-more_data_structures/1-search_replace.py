e1ac58f11cbb57bb8583145432b42750cb5abe46
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 04:52:58 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
+    for i in my_list:
+        if my_list[i] == search:
+            new_list[i] = replace
+        elif my_list[i] == replace:
+            new_list[i] = search
+        else:
+            new_list[i] = my_list[i]
+    return new_list
