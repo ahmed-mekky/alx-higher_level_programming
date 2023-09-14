c9e72a2ba4070412645b790b0b553b64419b9660
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 05:00:54 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    checklist = []
+    result = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in checklist:
+            result += my_list[i]
+            checklist.append(my_list[i])
+    return result
