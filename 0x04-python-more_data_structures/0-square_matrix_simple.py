32d01f0edb63ddb8b34555683d54634d79c2918b
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-14 04:48:29 +0300

    Python More Data Structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_arr = []
-    for row in matrix:
-        new_arr[row] = []
-        for col in row:
-            new_arr[col] = matrix[col] ** 2
+    rows = len(matrix)
+    cols = len(matrix[0] if rows > 0 else 0)
+    new_arr = [[None] * cols for x in range(rows)]
+    for row in range(rows):
+        for col in range(cols):
+            new_arr[row][col] = matrix[row][col] ** 2
     return new_arr
