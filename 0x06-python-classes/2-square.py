5cd0f98485ba72d3abbff17496acb53abea35588
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-29 13:35:14 +0300

    Python Classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python OOP"""
+
+
+class Square:
+    """class"""
+    def __init__(self, size):
+        """init fun"""
+        if type(size) is not int:
+            print("Size must be an integer")
+        if size < 0:
+            print('ValueError')
+        self.__size = size
