a0f275312945d7941ad3cc0f929c0b4ac5a98dba
ahmed-mekky <27medsh3pan@gmail.com>
2023-09-29 11:49:14 +0300

    Python Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """python OOP"""
 
+
 class Square:
     """empty class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """python OOP"""
 
+
 class Square:
     """class"""
     def __init__(self, size):
