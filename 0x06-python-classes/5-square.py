9149f8b46554e20e1a969ce003bf86d9ebf049a9
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-03 23:34:05 +0300

    Python Classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,10 +6,6 @@ class Square:
     """class"""
     def __init__(self, size=0):
         """init fun"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,14 +6,8 @@ class Square:
     """class"""
     def __init__(self, size=0, position=(0, 0)):
         """init fun"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if not isinstance(position, tuple) or position[1] < 0 or position[0] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
-        self.__position = position 
+        self.__position = position
 
     @property
     def size(self):
@@ -33,7 +27,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or value[1] < 0 or value[0] < 0:
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[1], int) or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -41,21 +39,14 @@ class Square:
         return (self.__size ** 2)
 
     def my_print(self):
-        i = self.__size
         if self.__size == 0:
             print()
-        x = self.__position[1]
-        while x:
+            return
+        for i in range(self.__position[1]):
             print()
-            x -= 1
-        while i > 0:
-            j = self.__size
-            y = self.__position[0]
-            while y > 0:
+        for j in range(self.size):
+            for j in range(self.__position[0]):
                 print(end=" ")
-                y -= 1
-            while j > 0:
+            for j in range(self.size):
                 print('#', end="")
-                j -= 1
             print()
-            i -= 1
