17c9de247fa104c87790f648309dd4e2a6239a13
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-03 22:46:21 +0300

    Python Classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""python OOP"""
+
+
+class Square:
+    """class"""
+    def __init__(self, size=0):
+        """init fun"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
+    
+    def my_print(self):
+        i ,j = self.__size
+        if self.__size == 0:
+            print()
+        while i > 0:
+            while j > 0:
+                print('#' end="")
+                j -= 1
+            print()
+            i -= 1
