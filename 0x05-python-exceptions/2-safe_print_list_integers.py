0fe9cad6aaa390b5bb805bc0fdc1ef98fde54df6
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-04 21:26:40 +0300

    Python Exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             if not isinstance(my_list[i], int):
                 raise TypeError
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             n += 1
         except TypeError:
             pass
