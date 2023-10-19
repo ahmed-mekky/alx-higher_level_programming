5e4eed7faff3cd52e8e44a241a0cafc8058fe6c7
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-20 01:46:32 +0300

    Python Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,8 +2,10 @@
 """
     idk
 """
+
+
 def get_attributes_and_methods(obj):
     """
-        This function returns a dictionary with the attributes and methods of an object.
+        Get all attributes and methods of an object.
     """
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    return dir(obj)
