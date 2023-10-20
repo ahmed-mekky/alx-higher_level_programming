ec5f661e417593725c3c63f69a4f7737a041a4c9
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-20 17:54:52 +0300

    Python Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-File name: 0-lookup.py
-"""
+"""File name: 0-lookup.py"""
 
 
 def get_attributes_and_methods(obj):
-    """
-        Get all attributes and methods of an object.
-    """
+    """Get all attributes and methods of an object."""
     return dir(obj)
