2104253d43c9e232416ee470a779b91ac7c4fb74
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-22 19:41:23 +0300

    Python Test Driven Development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,6 +17,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    a = round(a)
-    b = round(b)
+    a = int(a)
+    b = int(b)
     return (a + b)
