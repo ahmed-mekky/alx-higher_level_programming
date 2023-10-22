9d0bbce0bb9521d7effe31c452fab385be29c7a3
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-22 22:52:00 +0300

    Python Test Driven Development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,10 +9,19 @@ def matrix_divided(matrix, div):
     Raises:
     Return:
     """
-    new = [[0, 0, 0], [0, 0, 0]]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
+    num_rows = len(matrix)
+    num_columns = len(matrix[0])
+
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+
+    new_matrix = [[None for _ in range(num_columns)] for _ in range(num_rows)]
+
+    for i in range(num_rows):
+        if num_columns != len(matrix[i]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in range(num_columns):
             if type(matrix[i][j]) not in (int, float):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            new[i][j] = round((matrix[i][j] / div), 2)
-    return new
+            new_matrix[i][j] = round((matrix[i][j] / div), 2)
+    return new_matrix
