5156913e7a2497150879e0496d4a50ab1b5dc6da
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 03:43:11 +0200

    Python Test Driven Development

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Inheritance Modules
+"""
+
+
+def MyList(List):
+    """
+    This function adds something to the world.
+    """
+    def print_sorted(self):
+        print(sorted(self))
