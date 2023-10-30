925e9fdb24d9982394278e80ec87b1b18472d054
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 04:05:08 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Inheritance Modules
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    This thing adds something to the world.
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
