d52c6f6be31e24abbd71baf78e864205f98a157f
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 03:37:52 +0200

    Python Test Driven Development

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,5 @@ File name: 0-lookup.py
 
 
 def get_attributes_and_methods(obj):
-    """Get all attributes and methods of an object."""
+    """Get all attributes and methods of an object jejcnocnreomnrcmnlcmrdklfcnkecjkd."""
     return dir(obj)
