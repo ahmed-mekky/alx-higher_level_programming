4482f8c2660d19a4a06e20ac82ec9bef2156ccd2
ahmed-mekky <27medsh3pan@gmail.com>
2023-10-30 04:29:46 +0200

    Inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Inheritance Modules"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """This thing adds something to the world."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
