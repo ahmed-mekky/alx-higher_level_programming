7bf8df2c7c04cdf76e8edbde78976e506bb994cb
ahmed-mekky <27medsh3pan@gmail.com>
2023-11-17 04:57:47 +0200

    SQL

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
 -- SQL.
 SELECT name
 FROM tv_genres
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-AND tv_show_genres.show_id = 8
+LEFT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
