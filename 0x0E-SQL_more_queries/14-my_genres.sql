5631c1960f3bfe9495e82ca7a840cba86535962d
ahmed-mekky <27medsh3pan@gmail.com>
2023-11-17 04:45:49 +0200

    SQL

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- SQL.
-SELECT tv_shows.title AS 'title'
+SELECT tv_shows.title AS 'name'
 FROM tv_shows 
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show
+ON tv_shows.id = tv_show_genres.show_id
 AND tv_show_genres.genre_id = 5
 ORDER BY tv_shows.title;
