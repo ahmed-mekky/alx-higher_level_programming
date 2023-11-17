f3d702a48db053358f32c826c39cf2f5265b0af1
ahmed-mekky <27medsh3pan@gmail.com>
2023-11-17 04:44:38 +0200

    SQL

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title AS 'title'
 FROM tv_shows 
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show
+ON tv_shows.id = tv_show_genres.show_id
 AND tv_show_genres.genre_id = 5
 ORDER BY tv_shows.title;
