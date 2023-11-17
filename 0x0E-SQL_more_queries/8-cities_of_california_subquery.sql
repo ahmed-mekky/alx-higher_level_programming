2193d07bc34b7889f5bbc87c1d702b414e991466
ahmed-mekky <27medsh3pan@gmail.com>
2023-11-17 03:44:31 +0200

    SQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,10 @@
 -- SQL.
 SELECT cities.id, cities.name
-WHERE cities.id = (
-	SELECT states.id FROM states
-	WHERE states.name = 'California'
+FROM cities
+WHERE cities.state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
 	LIMIT 1
 )
 ORDER BY cities.id ASC;
