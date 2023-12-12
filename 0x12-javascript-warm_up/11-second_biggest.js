3e8d916de285f582452c3355673c1a441191b952
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-12 03:20:50 +0200

    JS

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+if (argv.length < 4) {
+  console.log(0);
+}
+let sec, first = argv[2];
+for (let i = 3; i < argv.length; i++) {
+  if (argv[i] > first) {
+    sec = first;
+    first = argv[i];
+  }
+}
+console.log(sec);
