126c22b665e01fa74adbf8a0fa05200b57244588
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-12 23:33:26 +0200

    JS

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,23 +3,21 @@ const argv = process.argv;
 
 if (argv.length < 4) {
   console.log(0);
-  return
-}
-
-const x = [];
-for (let i = 2; i < argv.length; i++) {
-  x[i - 2] = Number.parseInt(argv[i]);
-}
-
-const min = Math.min(...x);
-const max = Math.max(...x);
-
-
-let y = min;
-for (let i = 0; i < x.length; i++) {
-  if (x[i] > y && x[i] !== max) {
-    y = x[i];
-    console.log(y)
+} else {
+  const x = [];
+  for (let i = 2; i < argv.length; i++) {
+    x[i - 2] = Number.parseInt(argv[i]);
   }
+
+  const min = Math.min(...x);
+  const max = Math.max(...x);
+
+
+  let y = min;
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] > y && x[i] !== max) {
+      y = x[i];
+    }
+  }
+  console.log(y);
 }
-console.log(y);
