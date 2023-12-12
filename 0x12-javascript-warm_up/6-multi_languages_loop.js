299041d12b59dfca7e65b3ddcf0c88fa44a8c4ab
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-12 03:01:55 +0200

    JS

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let words = [
+const words = [
   'C is fun',
   'Python is cool',
   'JavaScript is amazing'
-]
+];
 for (let i = 0; i < words.length; i++) {
   console.log(words[i]);
 }
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const argv = process.argv;
+for (let i = 0; i < argv[2]; i++) {
+  console.log('C is fun');
+}
