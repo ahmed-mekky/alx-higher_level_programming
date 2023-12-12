1a25d0e70513c73b632bdd9a2cb16381dba77804
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-12 23:21:51 +0200

    JS

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (!argv[2]) {
+if (!Number.isInteger(parseInt(argv[2]))) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(argv[2]));
