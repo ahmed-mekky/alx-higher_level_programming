8891aa54a23b606f341618e5ea18a0a8054978a8
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-12 03:13:49 +0200

    JS

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv;
+
+if (typeof(argv[2]) != Number) {
+  console.log('Missing size')
+}
+
 for (let i = 0; i < argv[2]; i++) {
   for (let j = 0; j < argv[2]; j++) {
     process.stdout.write('X');
