19a6338231b1f54cc2bd56e225afd00400ceb44d
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-12 02:53:42 +0200

    JS

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const argv = process.argv;
 if (!argv[2]) {
   console.log('Not a number');
 } else {
-  parseInt('My number: ' + parseInt(argv[2]));
+  console.log('My number: ' + parseInt(argv[2]));
 }
