c455fee02d917ac566c42aca40af67341a4cff4b
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-13 04:05:15 +0200

    JS

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,19 +2,19 @@
 const argv = process.argv;
 const fs = require('fs');
 
-fs.readFile(argv[2], 'utf8', function(err, data1) {
+fs.readFile(argv[2], 'utf8', function (err, data1) {
   if (err) {
     return console.log(err);
   }
 
-  fs.readFile(argv[3], 'utf8', function(err, data2) {
+  fs.readFile(argv[3], 'utf8', function (err, data2) {
     if (err) {
       return console.log(err);
     }
 
-    let data = data1 + data2;
+    const data = data1 + data2;
 
-    fs.writeFile(argv[4], data, 'utf8', function(err) {
+    fs.writeFile(argv[4], data, 'utf8', function (err) {
       if (err) {
         return console.log(err);
       }
