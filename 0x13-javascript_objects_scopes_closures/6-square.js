b0b2d2631cb1a7fd4e864b14f812ff31992b038b
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-13 02:46:03 +0200

    JS

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c ? c : 'X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+}
+
+module.exports = Square;
