52eb0a2756316643c19057c76f6b471b36e410f2
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-13 02:55:41 +0200

    JS

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,10 +5,11 @@ class Square extends BaseSquare {
   constructor (size) {
     super(size, size);
   }
-  charPrint(c) {
+
+  charPrint (c) {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c ? c : 'X');
+        process.stdout.write(c || 'X');
       }
       process.stdout.write('\n');
     }
