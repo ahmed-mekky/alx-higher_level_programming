74a1981c9338e24923d466687afc55576cfad142
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-13 03:26:48 +0200

    JS

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  for (let i = 0; i < list / 2; i++) {
-    const temp = list[i];
+  for (let i = 0; i < (list.length / 2); i++) {
+    const tmp = list[i];
     list[i] = list[list.length - 1 - i];
-    list[list.length - 1 - i] = temp;
+    list[list.length - 1 - i] = tmp;
   }
   return list;
 };
