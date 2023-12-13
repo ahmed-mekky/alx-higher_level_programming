28adde596a8a7bbfb6d4add231c05cae0146b953
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-13 04:01:37 +0200

    JS

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const argv = process.argv;
+const fs = require('fs');
+
+fs.readFile(argv[2], 'utf8', function(err, data1) {
+  if (err) {
+    return console.log(err);
+  }
+
+  fs.readFile(argv[3], 'utf8', function(err, data2) {
+    if (err) {
+      return console.log(err);
+    }
+
+    let data = data1 + '\n' + data2;
+
+    fs.writeFile(argv[4], data, 'utf8', function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
+});
