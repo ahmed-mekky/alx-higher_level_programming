9b74a68e57c470f35e4728f420d1f45374bf22d6
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-18 04:50:19 +0200

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This script is doing something"""
 
-from sqlalchemy import Column, engine, String, Integer
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base()
+
 
 class State(Base):
     """A class representing a state in the US."""
     __tablename__ = "states"
 
-    name = Column(String(128), nullable=True)
+    name = Column(String(128), nullable=False)
     id = Column(Integer, primary_key=True)
