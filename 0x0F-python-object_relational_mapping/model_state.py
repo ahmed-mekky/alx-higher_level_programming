25f717c1c366b45eabc166263b8a515152c3f28f
ahmed-mekky <27medsh3pan@gmail.com>
2023-12-19 21:08:05 +0200

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,3 +14,6 @@ class State(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return(f"{self.id}: {self.name}")
