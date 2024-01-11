6ab6be39ddaa983ca214d37dc8c697e4e07ffbd0
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-12 01:23:14 +0200

    idk

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this module is doing something"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    owner = argv[2]
+    repo = argv[1]
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    r = requests.get(url)
+    json = r.json()
+
+    for i in range(10):
+        print(f"{json[i]['sha']}: {json[i]['commit']['author']['name']}")
