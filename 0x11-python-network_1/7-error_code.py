db5a4cb309f13e49081cfb3e9933dff5b51a8c49
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-12 00:36:48 +0200

    idk

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this module is doing something"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(argv[1])
+        print(r.text)
+    except requests.HTTPError as e:
+        print(f"Error code: {e.code}")
