c6da2c770e49cbd222a8bac991dfb0b41824694c
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-12 00:47:04 +0200

    idk

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     try:
         r = requests.get(argv[1])
-        print(r.text)
         r.raise_for_status()
-    except requests.exceptions.HTTPError as e:
+        print(r.text)
+    except requests.exceptions.HTTPError:
         print(f"Error code: {r.status_code}")
