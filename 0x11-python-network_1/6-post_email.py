73c3d902f71189d3705b997d5bec997f17450c95
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-12 00:11:55 +0200

    idk

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@ from sys import argv
 
 if __name__ == "__main__":
     data = {"email": argv[2]}
-    r = requests.post(argv[1], json=data)
+    r = requests.post(argv[1], data=data)
     print(r.text)
