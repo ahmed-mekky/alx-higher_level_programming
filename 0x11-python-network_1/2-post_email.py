754382bfe9a0cf85132baafa85c73afc5714a307
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-11 23:25:07 +0200

    web servers

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from urllib import request, parse
+from sys import argv
+"""this module is doing something"""
+
+import urllib.request
+import urllib.parse
+
+url = argv[1]
+data = {"email": argv[2]}
+data = urllib.parse.urlencode(data).encode()
+req = urllib.request.Request(url, data=data)
+
+with urllib.request.urlopen(req) as r:
+    print(r.read().decode("UTF-8"))
+
