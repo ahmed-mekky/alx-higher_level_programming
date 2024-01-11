c9a6739d16d2f467fd3caf897e906a9cda279c55
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-11 23:35:17 +0200

    web servers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@ from urllib import request
 from sys import argv
 """this module is doing something"""
 
-with request.urlopen(argv[1]) as r:
-    headers = r.headers
-    print(headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as r:
+        headers = r.headers
+        print(headers.get('X-Request-Id'))
