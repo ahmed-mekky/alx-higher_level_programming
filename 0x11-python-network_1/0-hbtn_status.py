b3f8fa8ee2fa5350fdef9e14cf0645c81969fa22
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-11 22:59:50 +0200

    web servers

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,5 @@ from urllib import request
 
 with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
     b_content = r.read()
-    print(f"""Body response:
-    - type: {type(b_content)}
-    - content: {b_content}
-    - utf8 content: {b_content.decode("UTF-8")}""")
+    print(f"Body response:\n\t- type: {type(b_content)}")
+    print(f"\t- content: {b_content}\n\t- utf8 content: {b_content.decode('UTF-8')}")
