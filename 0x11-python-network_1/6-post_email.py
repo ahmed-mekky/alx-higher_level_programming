09f777c6ff0e9d53509801f3686d5dbbf6ce6575
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-12 00:07:20 +0200

    idk

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module is doing something"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {"email": argv[2]}
+    r = requests.post(argv[1], json=data)
+    print(r.text)
