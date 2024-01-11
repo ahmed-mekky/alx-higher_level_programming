ac03b826f153801c17408ab264d06af3d4ee774e
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-12 00:56:09 +0200

    idk

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this module is doing something"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {"q": argv[1] if len(argv) == 1 else ""}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        r_json = r.json()
+        if not r_json:
+            print("No result")
+        else:
+            print(f"[{r_json.get('id')}] {r_json.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
