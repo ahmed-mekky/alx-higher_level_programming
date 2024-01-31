42005c9de92697d427bd518832e2331d3a392c2b
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-31 03:05:33 +0200

    javascript web scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-const { encode } = require('punycode');
 
 request.get(process.argv[2], (err, res, body) => {
   if (err) {
     return console.log(err);
   }
-  fs.writeFile(process.argv[3], body, encode, (err) => {
+  const encoding = 'utf8';
+  fs.writeFile(process.argv[3], body, { encoding }, (err) => {
     if (err) {
       throw err;
     }
   });
-  
 });
