61c20dd296d6d4797ebd400d0f872de854126d4d
ahmed-mekky <27medsh3pan@gmail.com>
2024-02-01 00:16:00 +0200

    javascript web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,11 +1,31 @@
 #!/usr/bin/node
 const request = require('request');
 
-request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res, body) => {
+request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], async (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
   const dict = JSON.parse(body);
-    dict['characters'].forEach(character => {
-      request.get(character, (err, res, body) => {
-        console.log(JSON.parse(body).name)
+  const characters = dict.characters;
+
+  for (const characterUrl of characters) {
+    try {
+      const characterResponse = await new Promise((resolve, reject) => {
+        request.get(characterUrl, (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(body);
+          }
+        });
       });
-    });
+
+      const character = JSON.parse(characterResponse);
+      console.log(character.name);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 });
