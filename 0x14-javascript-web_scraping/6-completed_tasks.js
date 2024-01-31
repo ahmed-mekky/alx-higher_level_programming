07a7d6628e131aa032d12074b1c415ecf3673a67
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-31 23:34:29 +0200

    javascript web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,17 +8,19 @@ request.get(process.argv[2], (err, res, body) => {
   const content = JSON.parse(body);
   const list = {};
   let count = 0;
-  let id = content[0].userId;
-  content.forEach(element => {
-    if (element.userId !== id) {
-      list[id] = count;
-      id = element.userId;
-      count = 0;
-    }
-    if (element.completed === true) {
-      count += 1;
-    }
-  });
-  list[id] = count;
+  if (content[0]) {
+    let id = content[0].userId;
+    content.forEach(element => {
+      if (element.userId !== id) {
+        list[id] = count;
+        id = element.userId;
+        count = 0;
+      }
+      if (element.completed === true) {
+        count += 1;
+      }
+    });
+    list[id] = count;
+  }
   console.log(list);
 });
