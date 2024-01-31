9198db58ecb30c938280abd7fafa6d8b1d12325c
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-31 02:56:36 +0200

    javascript web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ request.get(process.argv[2], (err, res, body) => {
   const dict = JSON.parse(body).results;
   Object.keys(dict).forEach(element => {
     dict[element].characters.forEach(character => {
-      if (character == 'https://swapi-api.alx-tools.com/api/people/18/') {
+      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
         x += 1;
       }
     });
