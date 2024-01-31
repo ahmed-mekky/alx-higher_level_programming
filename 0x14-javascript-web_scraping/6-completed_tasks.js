87235ae2a982df76c7e4c1d4bcc363589764a3a2
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-31 04:58:41 +0200

    javascript web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,30 @@ const request = require('request');
 
 request.get('https://jsonplaceholder.typicode.com/todos', (err, res, body) => {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
-  let content = JSON.parse(body)
-  let list = {}
-  let count = 0
-  let id = content[0].userId
-  content.forEach(element => {
-    if ( element.userId != id) {
-      list[id] = count
-      id = element.userId
-      count = 0
-    }
-    if( element.completed === true ) {
-      count += 1
-    }
-  });
-  console.log(list)
+  try {
+    const content = JSON.parse(body);
+    const list = {};
+    let count = 0;
+    let id = content[0].userId;
+
+    content.forEach((element) => {
+      if (element.userId !== id) {
+        list[id] = count;
+        id = element.userId;
+        count = 0;
+      }
+
+      if (element.completed === true) {
+        count += 1;
+      }
+    });
+
+    list[id] = count;
+
+    console.log(list);
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+  }
 });
