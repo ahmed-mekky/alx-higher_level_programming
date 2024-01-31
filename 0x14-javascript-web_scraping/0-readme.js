e1e5d091bd7a9dbb098388094355eccb79707e93
ahmed-mekky <27medsh3pan@gmail.com>
2024-01-31 02:06:21 +0200

    javascript web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function(err, data) {
-
-	if ( err ) {
-		throw err;
-	}
-	else
-		console.log(data)
-	})
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    throw err;
+  } else { console.log(data); }
+});
